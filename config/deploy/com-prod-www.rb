dc1391f848d572be84344997461214699d58c360
Will Bryant <will.bryant@gmail.com>
2014-04-08 23:36:45 +1200

    B#47676 set up prod deployment to WLG

--- config/deploy/com-prod-www.rb
+++ config/deploy/com-prod-www.rb
@@ -1,6 +1,9 @@
 set :application, 'com-prod-www'
 set :repository,  'git@git.powershop.co.nz:web/powershop-com.git'
-set :gateway,     'assl.powershop.co.nz'
 set :deploy_to,   "/apps/#{application}"
 
-role :web, 'assl1', 'assl2'
+role :web, 'assl1', 'assl2', 'com-gateway-wlg1', 'com-gateway-wlg2'
+
+set :gateway,
+  'assl.powershop.co.nz' => ['assl1', 'assl2'],
+  'gateway-wlg.powershop.co.nz' => ['com-gateway-wlg1', 'com-gateway-wlg2']
