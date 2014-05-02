4eedb046d5d6dc410d678c3746a10b80b4bee676
Will Bryant <will.bryant@gmail.com>
2014-05-02 16:25:24 +1200

    B#46868 task #18 update deploy recipes for new AKL gateways

--- config/deploy/com-prod-www.rb
+++ config/deploy/com-prod-www.rb
@@ -2,8 +2,8 @@ set :application, 'com-prod-www'
 set :repository,  'git@git.powershop.co.nz:web/powershop-com.git'
 set :deploy_to,   "/apps/#{application}"
 
-role :web, 'assl1', 'assl2', 'com-gateway-wlg1', 'com-gateway-wlg2'
+role :web, 'com-gateway-akl1', 'com-gateway-akl2', 'com-gateway-wlg1', 'com-gateway-wlg2'
 
 set :gateway,
-  'assl.powershop.co.nz' => ['assl1', 'assl2'],
+  'gateway-akl.powershop.co.nz' => ['com-gateway-akl1', 'com-gateway-akl2'],
   'gateway-wlg.powershop.co.nz' => ['com-gateway-wlg1', 'com-gateway-wlg2']
--- config/deploy/com-qa-www.rb
+++ config/deploy/com-qa-www.rb
@@ -1,6 +1,9 @@
 set :application, 'com-qa-www'
 set :repository,  'git@git.powershop.co.nz:web/powershop-com.git'
-set :gateway,     'wssl.powershop.co.nz'
 set :deploy_to,   "/apps/#{application}"
 
-role :web, 'com-gateway-wlg1', 'com-gateway-wlg2'
+role :web, 'com-gateway-akl1', 'com-gateway-akl2', 'com-gateway-wlg1', 'com-gateway-wlg2'
+
+set :gateway,
+  'gateway-akl.powershop.co.nz' => ['com-gateway-akl1', 'com-gateway-akl2'],
+  'gateway-wlg.powershop.co.nz' => ['com-gateway-wlg1', 'com-gateway-wlg2']
