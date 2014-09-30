843e7f3de06ccfa0f350d449e7e1fd01f26f0593
Thong Kuah <kuahyeow@gmail.com>
2014-09-30 21:33:23 +1300

    B#32718 Expose /tag.txt to show the currently deployed version

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :build_script, 'bundle exec jekyll build'
 namespace :deploy do
   before 'deploy', 'require_tag'
   before "deploy:finalize_update", "deploy:make_tag"
+  after  "deploy:make_tag",        "deploy:tag_text"
   before "deploy:finalize_update", "deploy:update_release_branch"
 
   %i[ start stop restart finalize_update ].each do |t|
@@ -19,6 +20,11 @@ namespace :deploy do
   desc "Make a TAG file showing which git tag has been released"
   task :make_tag do
     run "umask 02 && echo '#{branch}' > #{release_path}/TAG"
+  end
+
+  desc "Make a tag.txt file accesible for all to see what git tag has been released"
+  task :tag_text do
+    run "umask 02 && echo '#{branch}' > #{release_path}/_site/tag.txt"
   end
 
   desc "Update the last-release branch for the given application"
