f625d6533679da194ec3a9b06d1d80360b883a17
Thong Kuah <kuahyeow@gmail.com>
2016-05-17 15:47:35 +1200

    Remove old gateway configs - we use the SSH config file on each dev's laptop nowadays

--- config/deploy/com-qa-www.rb
+++ config/deploy/com-qa-www.rb
@@ -3,7 +3,3 @@ set :repository,  'git@git.powershop.co.nz:web/powershop-com.git'
 set :deploy_to,   "/apps/#{application}"
 
 role :web, 'com-gateway-akl1', 'com-gateway-akl2', 'com-gateway-wlg1', 'com-gateway-wlg2'
-
-set :gateway,
-  'gateway-akl.powershop.co.nz' => ['com-gateway-akl1', 'com-gateway-akl2'],
-  'gateway-wlg.powershop.co.nz' => ['com-gateway-wlg1', 'com-gateway-wlg2']
