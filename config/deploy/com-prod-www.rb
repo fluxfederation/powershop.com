d0d86a14403a95e162b5e41308f4d2c7b6a7c965
norman <norman.vourlat@powershop.co.nz>
2017-02-08 13:32:37 +1300

    Update deploy conf with new servers hosting powershop.com

--- config/deploy/com-prod-www.rb
+++ config/deploy/com-prod-www.rb
@@ -2,4 +2,4 @@ set :application, 'com-prod-www'
 set :repository,  'git@git.powershop.co.nz:web/powershop-com.git'
 set :deploy_to,   "/apps/#{application}"
 
-role :web, 'com-gateway-akl1', 'com-gateway-akl2', 'com-gateway-wlg1', 'com-gateway-wlg2'
+role :web, 'hq-gateway-akl1', 'hq-gateway-akl2', 'hq-gateway-wlg1', 'hq-gateway-wlg2'
